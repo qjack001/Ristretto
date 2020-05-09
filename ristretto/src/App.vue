754f5f64ec696edc0c2e88331cda8c7be41d2f97
Jack Guinane <jackguinane@gmail.com>
2020-05-09 19:55:31 -0400

    add text editor

---
+++ ristretto/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="app">
+		<NavBar />
+		<TextEditor />
+	</div>
+</template>
+
+<script>
+	import NavBar from './components/NavBar.vue'
+	import TextEditor from './components/TextEditor.vue'
+
+	export default 
+	{
+		name: 'App',
+		components: 
+		{
+			NavBar,
+			TextEditor
+		}
+	}
+</script>
