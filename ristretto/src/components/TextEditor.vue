48d9d0b1df5d9033735206a59aef9996b7a6f8ee
Jack Guinane <jackguinane@gmail.com>
2020-05-10 02:37:14 -0400

    add placeholder text, image support, hr support, and trailing node

--- ristretto/src/components/TextEditor.vue
+++ ristretto/src/components/TextEditor.vue
@@ -1,36 +1,38 @@
 <template>
 	<div class="editor">
-		<editor-menu-bubble :editor="editor" :keep-in-bounds="keepInBounds" v-slot="{ commands, isActive, menu }">
+		<editor-menu-bubble :editor="editor" :keep-in-bounds="keepInBounds" @hide="hideLinkMenu" v-slot="{ commands, isActive, getMarkAttrs, menu }">
 			<div class="menububble" :class="{ 'is-active': menu.isActive }" :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`">
-				
-		
+                <form class="menububble__form" v-if="linkMenuIsActive" @submit.prevent="setLinkUrl(commands.link, linkUrl)">
+                    <input class="menububble__input" type="text" v-model="linkUrl" placeholder="https://" ref="linkInput" @keydown.esc="hideLinkMenu"/>
+                    <button class="menububble__button" @click="setLinkUrl(commands.link, null)" type="button">
+                        <icon name="remove" />
+                    </button>
+                </form>
+                <template v-else>
+                    <button class="menububble__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+                        <icon name="bold">bold</icon>
+                    </button>
 
-				<button class="menububble__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
-					bold
-					<icon name="bold" />
-				</button>
+                    <button class="menububble__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+                        <icon name="italic">italic</icon>
+                    </button>
 
-				<button class="menububble__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
-					italic
-					<icon name="italic" />
-				</button>
+                    <button class="menububble__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
+                        <icon name="italic">underline</icon>
+                    </button>
 
-				<button class="menububble__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
-					underline
-					<icon name="italic" />
-				</button>
+                    <button class="menububble__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike">
+                        <icon name="italic">strike</icon>
+                    </button>
 
-				<button class="menububble__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike">
-					strike
-					<icon name="italic" />
-				</button>
+                    <button class="menububble__button" :class="{ 'is-active': isActive.code() }" @click="commands.code">
+                        <icon name="code">code</icon>
+                    </button>
 
-				<button class="menububble__button" :class="{ 'is-active': isActive.code() }" @click="commands.code">
-					code
-					<icon name="code" />
-				</button>
-
-
+                    <button class="menububble__button" :class="{ 'is-active': isActive.link() }" @click="showLinkMenu(getMarkAttrs('link'))">
+                        <icon name="link">link</icon>
+                    </button>
+                </template>
 			</div>
 		</editor-menu-bubble>
 		<editor-content class="editor__content" :editor="editor" />
@@ -48,8 +50,6 @@
 		Heading,
 		ListItem,
 		OrderedList,
-		TodoItem,
-		TodoList,
 		Bold,
 		Code,
 		Italic,
@@ -57,7 +57,10 @@
 		Strike,
 		Underline,
 		History,
-		HorizontalRule,
+        HorizontalRule,
+        Image,
+        TrailingNode,
+        Placeholder,
 	} from 'tiptap-extensions'
 
 	export default 
@@ -81,8 +84,6 @@
 						new Heading({ levels: [1, 2, 3] }),
 						new ListItem(),
 						new OrderedList(),
-						new TodoItem(),
-						new TodoList(),
 						new Link(),
 						new Bold(),
 						new Code(),
@@ -90,18 +91,47 @@
 						new Strike(),
 						new Underline(),
 						new History(),
-						new HorizontalRule(),
+                        new HorizontalRule(),
+                        new Image(),
+                        new TrailingNode({
+                            node: 'paragraph',
+                            notAfter: ['paragraph'],
+                        }),
+                        new Placeholder({
+                            emptyEditorClass: 'is-editor-empty',
+                            emptyNodeClass: 'is-empty',
+                            emptyNodeText: 'Write something...',
+                            showOnlyWhenEditable: true,
+                            showOnlyCurrent: true,
+                        }),
 					],
-					content: `
-						<h2>
-						Menu Bubble
-						</h2>
-						<p>
-						Hey, try to select some text here. There will popup a menu for selecting some inline styles. <em>Remember:</em> you have full control about content and styling of this menu.
-						</p>
-					`,
-				})
-			}
+					content: ``,
+                    
+                }),
+                linkUrl: null,
+                linkMenuIsActive: false,
+            }
+        },
+        methods:
+        {
+            showLinkMenu(attrs)
+            {
+                this.linkUrl = attrs.href
+                this.linkMenuIsActive = true
+                this.$nextTick(() => {
+                    this.$refs.linkInput.focus()
+                })
+            },
+            hideLinkMenu()
+            {
+                this.linkUrl = null
+                this.linkMenuIsActive = false
+            },
+            setLinkUrl(command, url)
+            {
+                command({ href: url })
+                this.hideLinkMenu()
+            }
 		},
 		beforeDestroy() 
 		{
