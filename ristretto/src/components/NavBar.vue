8a33bd0caf10a6723d955b1576bf8df6a2ea09bc
Jack Guinane <jackguinane@gmail.com>
2020-05-10 02:36:12 -0400

    add settings and markdown guide

--- ristretto/src/components/NavBar.vue
+++ ristretto/src/components/NavBar.vue
@@ -4,11 +4,44 @@
 			<img class="logo" src="../assets/logo.png" width="24px" height="24px">
 			<h1 class="logo">Ristretto</h1>
 		</a>
-		<button>
+		<button :class="getSettingsVisible ? 'clicked' : ''" @click="showSettings">
 			<svg height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/>
 				<path d="M19.44 12.99l-.01.02c.04-.33.08-.67.08-1.01s-.03-.66-.07-.99l.01.02 2.44-1.92-2.43-4.22-2.87 1.16.01.01c-.52-.4-1.09-.74-1.71-1h.01L14.44 2H9.57l-.44 3.07h.01c-.62.26-1.19.6-1.71 1l.01-.01-2.88-1.17-2.44 4.22 2.44 1.92.01-.02c-.04.33-.07.65-.07.99s.03.68.08 1.01l-.01-.02-2.1 1.65-.33.26 2.43 4.2 2.88-1.15-.02-.04c.53.41 1.1.75 1.73 1.01h-.03L9.58 22h4.85s.03-.18.06-.42l.38-2.65h-.01c.62-.26 1.2-.6 1.73-1.01l-.02.04 2.88 1.15 2.43-4.2s-.14-.12-.33-.26zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"/>
 			</svg>
 		</button>
+
+		<div :class="getSettingsVisible ? 'settings open' : 'settings closed'">
+			<div class="full-height">
+				<section>
+					<p><em>a small but mighty notepad.</em></p>
+				</section>
+				<section>
+					<h3>Theme</h3>
+					<button>1</button>
+					<button>2</button>
+					<button>3</button>
+				</section>
+				<section>
+					<h3>Markdown Guide</h3>
+					<div class="grid">
+						<p>Heading 1:</p> <p><code>#</code> <code>Space</code></p>
+						<p>Heading 2:</p> <p><code>##</code> <code>Space</code></p>
+						<p>Heading 3:</p> <p><code>###</code> <code>Space</code></p>
+						<p><strong>Bold:</strong></p> <p><code>**bold**</code></p>
+						<p><em>Italic:</em></p> <p><code>*italic*</code></p>
+						<p><s>Strikethrough:</s></p> <p><code>~strikethrough~</code></p>
+						<p><u>Underline:</u></p> <p><code>⌘</code> + <code>U</code></p>
+						<p><code>Code:</code></p> <p><code>`code`</code></p>
+						<p><code>Codeblock:</code></p> <p><code>```</code></p>
+						<blockquote>Blockquote:</blockquote> <p><code>></code> <code>Space</code></p>
+						<p><ul><li>Unordered list:</li></ul></p> <p><code>-</code> <code>Space</code></p>
+						<p><ol><li>Ordered list:</li></ol></p> <p><code>1.</code> <code>Space</code></p>
+						<p>Image:</p> <p><code>![](https://...)</code></p>
+						<p>Horizontal rule:</p> <p><code>---</code></p>
+					</div>
+				</section>
+			</div>
+		</div>
 	</header>
 </template>
 
@@ -18,7 +51,8 @@ export default
 	data() 
 	{
 		return {	
-			scrollPosition: 0
+			scrollPosition: 0,
+			settingsVisible: false
         }
 	},
 	mounted() 
@@ -29,7 +63,11 @@ export default
 	{
 		updateScroll: function() 
 		{
-			this.scrollPosition = window.scrollY
+			this.scrollPosition = window.scrollY;
+		},
+		showSettings: function()
+		{
+			this.settingsVisible = !this.settingsVisible;
 		}
 	},
 	computed:
@@ -37,7 +75,15 @@ export default
 		getScroll: function()
 		{
 			return this.scrollPosition;
+		},
+		getSettingsVisible: function()
+		{
+			return this.settingsVisible;
 		}
+	},
+	beforeDestroy() 
+	{
+		window.removeEventListener('scroll', this.updateScroll);
 	}
 }
 </script>
